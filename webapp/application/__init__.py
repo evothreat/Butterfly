d13e903eea0a8d5eff660c5e97c5fbb885c4d11a
cihasev <cihasev@uni-bremen.de>
2021-08-08 17:29:26 +0200

    configs exported to file

--- webapp/application/__init__.py
+++ webapp/application/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_pyfile('../config.py')  # load config in run.py
 
 db = SQLAlchemy(app)
