71018f6ddaf0d5a92fc7634a5660a33eb1f71c17
cihasev <cihasev@uni-bremen.de>
2021-08-08 20:55:17 +0200

    last state

--- webapp/application/models.py
+++ webapp/application/models.py
@@ -1,29 +1,57 @@
+import datetime
 from application import db
+from dataclasses import dataclass
+from sqlalchemy.sql import func
 
 
-class ResourceInfo(db.Model):
+@dataclass
+class Job(db.Model):
+    id: int
+    args: str
+    completed: bool
+    timestamp: datetime.datetime
+    worker_id: int
+
     id = db.Column(db.Integer, primary_key=True)
-    gpu = db.Column(db.String(50))
-    cpu = db.Column(db.String(50))
-    ram = db.Column(db.String(10))                                          # maybe use integer?
+    args = db.Column(db.String(250))
+    completed = db.Column(db.Boolean(), default=False)
+    timestamp = db.Column(db.DateTime(), default=func.now())
     worker_id = db.Column(db.Integer, db.ForeignKey('worker.id'))
 
 
+@dataclass
+class ResourceInfo(db.Model):
+    id: int
+    gpu: str
+    cpu: str
+    ram: str
+    worker_id: int
+
+    id = db.Column(db.Integer, primary_key=True)
+    gpu = db.Column(db.String(50))
+    cpu = db.Column(db.String(50))
+    ram = db.Column(db.String(10))  # maybe use integer?
+    worker_id = db.Column(db.Integer, db.ForeignKey('worker.id'))
+
+
+@dataclass
 class Worker(db.Model):
+    id: int
+    hostname: str
+    os: str
+    country: str
+    ip_addr: str
+    mac_addr: str
+    last_seen: datetime.datetime
+    resource_info: ResourceInfo
+    jobs: Job
+
     id = db.Column(db.Integer, primary_key=True)
     hostname = db.Column(db.String(30))
     os = db.Column(db.String(30))
     country = db.Column(db.String(30))
     ip_addr = db.Column(db.String(15))
-    mac_addr = db.Column(db.String(17), unique=True)                                     # set as primary key?
-    last_seen = db.Column(db.DateTime())
-    resource_info = db.relationship("ResourceInfo", uselist=False)
-    jobs = db.relationship("Job")
-
-
-class Job(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    args = db.Column(db.String(250))
-    completed = db.Column(db.Boolean())
-    timestamp = db.Column(db.DateTime())
-    worker_id = db.Column(db.Integer, db.ForeignKey('worker.id'))
+    mac_addr = db.Column(db.String(17), unique=True)  # set as primary key?
+    last_seen = db.Column(db.DateTime(), default=func.now())
+    resource_info = db.relationship("ResourceInfo", uselist=False, cascade="all, delete-orphan")
+    jobs = db.relationship("Job", cascade="all, delete-orphan")
--- webapp/application/views.py
+++ webapp/application/views.py
@@ -1,6 +1,42 @@
-from application import app
+from application import *
+from application.models import *
+from flask import jsonify
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/api/v1/workers', methods=['GET'])
+def get_workers():
+    return jsonify(Worker.query.all()), 200
+
+
+@app.route('/api/v1/workers', methods=['POST'])
+def create_worker():
+    pass
+
+
+@app.route('/api/v1/workers/<int:wid>', methods=['GET'])
+def get_single_worker(wid):
+    w = Worker.query.get(wid)
+    return ('', 404) if not w else (jsonify(w), 200)
+
+
+@app.route('/api/v1/workers/<int:wid>', methods=['DELETE'])
+def delete_worker(wid):
+    Worker.query.filter_by(id=wid).delete()
+    db.session.commit()
+    return '', 200
+
+
+@app.route('/api/v1/workers/<int:wid>/jobs', methods=['GET'])
+def get_jobs(wid):
+    w = Worker.query.get(wid)
+    return ('', 404) if not w else (jsonify(w.jobs), 200)
+
+
+@app.route('/api/v1/workers/<int:wid>/jobs', methods=['POST'])
+def create_job(wid):
+    pass
