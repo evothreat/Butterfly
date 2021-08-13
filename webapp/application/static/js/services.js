58d4a84014222f395dbb0e8a33cf8acb8f20a8a4
cihasev <cihasev@uni-bremen.de>
2021-08-13 17:14:22 +0200

    own modal window implemented

--- webapp/application/static/js/services.js
+++ webapp/application/static/js/services.js
@@ -3,7 +3,7 @@ var resourceInfo;
 
 function loadResourceInfo() {
     $.getJSON('/api/v1/workers/-/resource-info', function (res) {
-        resourceInfo = res.reduce(function (map, ri) {            // TODO: use array with filter?
+        resourceInfo = res.reduce(function (map, ri) {
             map[ri.worker_id] = ri;
             return map;
         }, {});
@@ -15,5 +15,5 @@ function showResourceInfo(wid) {
     $('#cpu-info').html(ri.cpu);
     $('#gpu-info').html(ri.gpu);
     $('#ram-info').html(ri.ram);
-    $('#resource-dlg').modal();
+    $('#resource-dlg').css('display', 'flex');
 }
