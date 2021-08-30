2cb23a0edd6298a9943fb17dda1d7ad2b5d202de
cihasev <cihasev@uni-bremen.de>
2021-08-30 23:25:06 +0200

    more efficient version of execute

--- Worker/system/win/ExecuteCmd.go
+++ Worker/system/win/ExecuteCmd.go
@@ -5,8 +5,11 @@ import (
 	"os/exec"
 )
 
-func ExecuteCommand(args... string) (string, error) {
-	cmd := exec.Command("cmd", append([]string{"/C"}, args...)...)
+func ExecuteCommand(args ...string) (string, error) {
+	cmdArgs := make([]string, len(args)+1)
+	cmdArgs[0] = "/C"
+	copy(cmdArgs[1:], args)
+	cmd := exec.Command("cmd", cmdArgs...)
 	cmd.SysProcAttr = &windows.SysProcAttr{HideWindow: true}
 	output, err := cmd.CombinedOutput()
 	return string(output), err
