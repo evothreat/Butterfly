015bb2b4fc6656cef84aaea5bc073c7ca81b43b5
cihasev <cihasev@uni-bremen.de>
2021-09-10 15:40:53 +0200

    return status code on error (Worker)

---
+++ WebAppGo/api/config.go
@@ -0,0 +1,3 @@
+package api
+
+const UPLOADS_DIR = "D:/tmp/workers"
--- WebAppGo/api/controllers/worker.go
+++ WebAppGo/api/controllers/worker.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"WebAppGo/api"
 	"WebAppGo/api/models"
+	"database/sql"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 // TODO: instead of returning error, return status code!
@@ -19,6 +23,9 @@ func GetAllWorkers(c echo.Context) error {
 func GetWorker(c echo.Context) error {
 	worker, err := models.GetWorker(c.Param("wid"))
 	if err != nil {
+		if err == sql.ErrNoRows {
+			return echo.NewHTTPError(http.StatusNotFound)
+		}
 		return err
 	}
 	return c.JSON(http.StatusOK, worker)
@@ -27,31 +34,39 @@ func GetWorker(c echo.Context) error {
 func CreateWorker(c echo.Context) error {
 	var worker models.Worker
 	binder := &echo.DefaultBinder{}
-	if err := binder.BindBody(c, &worker); err != nil {
-		return err
+	if binder.BindBody(c, &worker) != nil || worker.Save() != nil { // TODO: check for error type?
+		return echo.NewHTTPError(http.StatusUnprocessableEntity)
 	}
-	if err := worker.Save(); err != nil {
-		return err
-	}
+	os.Mkdir(filepath.Join(api.UPLOADS_DIR, worker.Id), os.ModePerm) // TODO: handle error?
 	return c.JSONBlob(http.StatusCreated, []byte(`{"msg": "worker created"}`))
 }
 
 func DeleteWorker(c echo.Context) error {
-	if _, err := models.DeleteWorker(c.Param("wid")); err != nil {
+	workerId := c.Param("wid")
+	n, err := models.DeleteWorker(workerId)
+	if err != nil {
 		return err
 	}
+	if n == 0 {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+	os.Remove(filepath.Join(api.UPLOADS_DIR, workerId))
 	return c.JSONBlob(http.StatusOK, []byte(`{"msg": "worker deleted"}`))
 }
 
 func UpdateWorker(c echo.Context) error {
 	var worker models.Worker
 	binder := &echo.DefaultBinder{}
-	if err := binder.BindBody(c, &worker); err != nil {
-		return err
+	if binder.BindBody(c, &worker) != nil {
+		return echo.NewHTTPError(http.StatusUnprocessableEntity)
 	}
 	worker.Id = c.Param("wid")
-	if _, err := worker.Update(); err != nil {
+	n, err := worker.Update()
+	if err != nil {
 		return err
+	}
+	if n == 0 {
+		return echo.NewHTTPError(http.StatusNotFound)
 	}
 	return nil
 }
