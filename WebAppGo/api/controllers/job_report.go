0fa8398a3aa89e1af14764998aab4df2b41d690a
cihasev <cihasev@uni-bremen.de>
2021-09-19 21:45:55 +0200

    small change

--- WebAppGo/api/controllers/job_report.go
+++ WebAppGo/api/controllers/job_report.go
@@ -45,8 +45,7 @@ func CreateReport(c echo.Context) error {
 func GetReport(c echo.Context) error {
 	jobId, _ := strconv.Atoi(c.Param("jid"))
 	var report string
-	stmt := "SELECT job_reports.report FROM job_reports INNER JOIN jobs ON job_reports.job_id=jobs.id WHERE jobs.worker_id=? AND job_reports.job_id=?"
-	row := db.QueryRow(stmt, c.Param("wid"), jobId)
+	row := db.QueryRow("SELECT job_reports.report FROM job_reports INNER JOIN jobs ON job_reports.job_id=jobs.id WHERE jobs.worker_id=? AND job_reports.job_id=?", c.Param("wid"), jobId)
 	if err := row.Scan(&report); err != nil {
 		if err == sql.ErrNoRows {
 			return c.NoContent(http.StatusNotFound)
@@ -58,9 +57,13 @@ func GetReport(c echo.Context) error {
 
 func DeleteReport(c echo.Context) error {
 	jobId, _ := strconv.Atoi(c.Param("jid"))
-	stmt := "DELETE job_reports FROM job_reports INNER JOIN jobs ON job_reports.job_id=jobs.id WHERE jobs.worker_id=? AND job_reports.job_id=?"
-	if _, err := db.Exec(stmt, c.Param("wid"), jobId); err != nil {
+	res, err := db.Exec("DELETE job_reports FROM job_reports INNER JOIN jobs ON job_reports.job_id=jobs.id WHERE jobs.worker_id=? AND job_reports.job_id=?",
+		c.Param("wid"), jobId)
+	if err != nil {
 		return err
+	}
+	if n, _ := res.RowsAffected(); n == 0 {
+		return c.NoContent(http.StatusNotFound)
 	}
 	return c.NoContent(http.StatusOK)
 }
