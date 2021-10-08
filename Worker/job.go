9d602857fbb049ae4b5ef771855ede1d2d9ca0a7
cihasev <cihasev@uni-bremen.de>
2021-10-08 14:50:21 +0200

    rest file

--- Worker/job.go
+++ Worker/job.go
@@ -13,64 +13,6 @@ type Job struct {
 	Created time.Time `json:"created"`
 }
 
-type JobType int
-
-const (
-	UNKNOWN JobType = iota
-	SHELL_CMD
-	UPLOAD
-	DOWNLOAD
-	CHDIR
-	SLEEP
-	BOOST
-	SCREENSHOT
-	KEYLOGGER
-	DDOS
-	CREDENTIALS
-	MSG
-)
-
-type JobTypeInfo struct {
-	jtype JobType
-	argsN int
-	// maybe also add handler function
-}
-
-var jobTypesMap = map[string]*JobTypeInfo{
-	"cmd": {
-		jtype: SHELL_CMD,
-		argsN: -1,
-	},
-	"upload": {
-		jtype: UPLOAD,
-		argsN: 1,
-	},
-	"download": {
-		jtype: DOWNLOAD,
-		argsN: 2,
-	},
-	"sleep": {
-		jtype: SLEEP,
-		argsN: 1,
-	},
-	"boost": {
-		jtype: BOOST,
-		argsN: 1,
-	},
-	"chdir": {
-		jtype: CHDIR,
-		argsN: 1,
-	},
-	"msg": {
-		jtype: MSG,
-		argsN: 2,
-	},
-	"shot": {
-		jtype: SCREENSHOT,
-		argsN: 0,
-	},
-}
-
 func (j *Job) parse() (JobType, []string) {
 	values := utils.SplitArgsStr(j.Todo)
 	if len(values) == 0 {
